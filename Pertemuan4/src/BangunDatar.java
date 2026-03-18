d72b3b51ebc41a39f544777437f9d8369b16cbbe
Farras Hilmy Zaidan <farrashilmy12345@gmail.com>
2026-03-19 02:06:46 +0700

    add bangun datar

---
+++ Pertemuan4/src/BangunDatar.java
@@ -0,0 +1,68 @@
+/* Nama File    : BangunDatar.java
+ * Deskripsi    : berisi atribut dan method dalam class Bangun Datar
+ * Pembuat      : Farras Hilmy Zaidan - 24060124120003
+ * Tanggal      : 19 Maret 2026
+*/
+
+public class BangunDatar {
+    /***ATRIBUT***/
+    protected int jmlSisi; // menunjukkan jumlah sisi bangun datar
+    protected String warna; // menunjukkan warna yang ada pada bangun datar 
+    protected String border; // menunjukkan border pada bangun datar
+    protected static int counterBangunDatar = 0; // atribut static untuk menghitung bangun datar yang terbentuk
+
+    /***METHOD***/
+    /***KONSTRUKTOR***/
+    // konstruktor tanpa parameter
+    public BangunDatar() {
+        counterBangunDatar++;
+    }
+    // konstruktor dengan parameter
+    public BangunDatar(int jmlSisi, String warna, String border) {
+        this.jmlSisi = jmlSisi;
+        this.warna = warna;
+        this.border = border;
+        counterBangunDatar++;
+    }
+    /***SELEKTOR***/
+    // selektor jumlah sisi
+    public int getJmlSisi() {
+        return jmlSisi;
+    }
+    // selektor warna
+    public String getWarna() {
+        return warna;
+    }
+    // selektor Border
+    public String getBorder() {
+        return border;
+    }
+    /***MUTATOR***/
+    // mutator jumlah sisi
+    public void setJmlSisi(int jmlSisi) {
+        this.jmlSisi = jmlSisi;
+    }
+    // mutator warna
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    // mutator border
+    public void setBorder(String border) {
+        this.border = border;
+    }
+    /***OPERATOR TAMBAHAN***/
+    // menampilkan info lengkap sebuah bangun datar
+    public void printInfo() {
+        System.out.println("Jumlah sisi: " + jmlSisi);
+        System.out.println("Warna: " + warna);
+        System.out.println("Border: " + border);
+    }
+    // mengembalikan jumlah objek bangun datar yang berhasil terbentuk
+    public static int getCounterBangunDatar() {
+        return counterBangunDatar;
+    }
+    // menampilkan jumlah objek bangun datar yang sudah tercetak 
+    public static void printCounterBangunDatar() {
+        System.out.println("Jumlah objek: " + BangunDatar.getCounterBangunDatar());
+    }
+}
---
+++ Pertemuan4/src/Persegi.java
@@ -0,0 +1,74 @@
+/* Nama File    : Persegi.java
+ * Deskripsi    : berisi atribut dan method dalam class Persegi yang merupakan turunan dari Bangun Datar
+ * Pembuat      : Farras Hilmy Zaidan - 24060124120003
+ * Tanggal      : 19 Maret 2026
+*/
+
+public class Persegi extends BangunDatar {
+    /***ATRIBUT***/
+    private double sisi; // menunjukkan panjang sisi persegi
+    private static int counterPersegi = 0; // atribut static untuk menghitung objek persegi yang berhasil terbentuk
+
+    /***METHOD***/
+    /***KONSTRUKTOR***/
+    // konstruktor tanpa parameter
+    public Persegi() {
+        super(); // memanggil konstruktor BangunDatar
+        this.jmlSisi = 4; // bisa karena habis diupdate jadi protected dan masih berada di satu folder yakni src
+        counterPersegi++;
+    }
+
+    // konstruktor dengan parameter
+    public Persegi(double sisi, String warna, String border) {
+        super(4, warna, border); // memanggil konstruktor superclass
+        this.sisi = sisi;
+        counterPersegi++;
+    }
+
+    /***SELEKTOR***/
+    // selektor sisi
+    public double getSisi() {
+        return sisi;
+    }
+
+    // mutator sisi
+    public void setSisi(double sisi) {
+        this.sisi = sisi;
+    }
+
+    /***OPERATOR TAMBAHAN***/
+    // menghitung luas persegi
+    public double getLuas() {
+        return sisi * sisi;
+    }
+
+    // menghitung keliling persegi
+    public double getKeliling() {
+        return 4 * sisi;
+    }
+
+    // menghitung panjang diagonal persegi
+    public double getDiagonal() {
+        return Math.sqrt(2*(sisi*sisi));
+    }
+    
+    // mengembalikan banyaknya objek persegi
+    public static int getCounterPersegi() {
+        return counterPersegi;
+    }
+
+    // menampilkan banyaknya objek persegi
+    public static void printCounterPersegi() {
+        System.out.println("Jumlah Objek Persegi: " + Persegi.getCounterPersegi());
+    }
+
+    // menampilkan info lengkap persegi
+    @Override
+    public void printInfo() {
+        super.printInfo(); // memanggil method dari superclass
+        System.out.println("Sisi: " + sisi);
+        System.out.println("Luas: " + getLuas());
+        System.out.println("Keliling: " + getKeliling());
+        System.out.println("Diagonal: " + getDiagonal());
+    }
+}
