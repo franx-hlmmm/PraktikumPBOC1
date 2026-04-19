05494a4e0daf36934a52fe57fffe2d2db3fe83e0
Farras Hilmy Zaidan <farrashilmy12345@gmail.com>
2026-04-19 22:31:06 +0700

    menambahkan jawaban 3.2 dan 3.4 pada AngkaSial dan Asersi2

--- Pertemuan6/src/AngkaSial.java
+++ Pertemuan6/src/AngkaSial.java
@@ -1,6 +1,6 @@
 /**
  * File         : AngkaSial.java
- * Deskripsi    : Program penggunaan exception buatan sendiri
+ * Deskripsi    : Program penggunaan exception buatan sendiri pengenalan throw and throws
  * Pembuat      : Farras Hilmy Zaidan - 24060124120003
  * Tanggal      : 14 April 2026
  */
@@ -9,8 +9,7 @@ public class AngkaSial {
     public void cobaAngka(int angka) throws AngkaSialException {
         if (angka == 13) {
             throw new AngkaSialException();
-        }
-        System.out.println(angka + " bukan angka sial");
+        } System.out.println(angka + " bukan angka sial");
     }
 
     public static void main(String[] args) {
@@ -26,3 +25,25 @@ public class AngkaSial {
         }
     }
 }
+
+/**
+ * Baris 12, System.out.println(angka + " bukan angka sial");, akan dieksekusi jika angka
+ * yang dimasukkan bukan 13. Baris 21, catch (AngkaSialException ase), akan dieksekusi jika
+ * angka yang dimasukkan adalah 13. getMessage() akan mengembalikan pesan yang sudah
+ * didefinisikan pada kelas Exception, serta akan menampilkan output "hati-hati dalam
+ * memasukan angka!!!".
+ * 
+ * Hal ini terjadi karena pada blok try terdapat pemanggilan as.cobaAngka(13);, yang akan
+ * memanggil method cobaAngka(). Method cobaAngka() ini akan melempar exception
+ * AngkaSialException() jika angka yang dimasukkan adalah 13, sehingga program akan
+ * menangkap exception tersebut dan menampilkan pesan yang sudah didefinisikan. Lalu baris
+ * kode pada blok catch akan dieksekusi.
+ * 
+ * Baris dengan kode as.cobaAngka(12); tidak akan dieksekusi , karena baris kode sebelumnya yakni
+ * angka 13 memicu eksepsi, sehingga program akan langsung mengeksekusi blok catch dan
+ * berhenti pada baris kode tersebut. Sehingga, output yang dihasilkan ketika program dijalankan
+ * adalah:
+ * 10 bukan angka sial
+ * jangan memasukkan angka 13 karena angka sial!!!
+ * hati-hati dalam memasukkan angka!!!
+*/
